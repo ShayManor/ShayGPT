ff799d85fa4eb6523adfb0a934510f195cd9a568
shay <shay.manor@gmail.com>
2025-05-06 15:46:36 -0400

    lower batch_size

--- src/train.py
+++ src/train.py
@@ -28,7 +28,7 @@ def get_args():
                    default=20)
     p.add_argument('--batch_size',
                    type=int,
-                   default=32)
+                   default=16)
     p.add_argument('--lr',
                    type=float,
                    default=1.5e-4)
