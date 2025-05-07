a165481ad4de7de53100fa14fe6553545311818d
shay <shay.manor@gmail.com>
2025-05-07 16:03:59 -0400

    batch_size

--- src/test.py
+++ src/test.py
@@ -6,10 +6,10 @@ cfg = GPTConfig(vocab_size=50_000)
 model = GPT(cfg)
 # total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 # print(f"Total parameters: {total_params:,} ~= {total_params / 1e6:.2f} M parameters")
-model.load_state_dict(torch.load("data/checkpoint3.pth", map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("data/checkpoint4.pth", map_location=torch.device('cpu')))
 model.eval()
 
-prompt = "How am I feeling about my AP test tomorrow? "
+prompt = "10 words you would use to describe itai are"
 ids = tokenizer.encode(prompt).ids
 ids.insert(0, BOS_ID)
 for _ in range(25):
@@ -19,7 +19,7 @@ for _ in range(25):
     logits[0, sep_id] = -1e9
 
     next_id = torch.multinomial(
-        torch.softmax(logits / 0.4, dim=-1), num_samples=1).item()
+        torch.softmax(logits / 10, dim=-1), num_samples=1).item()
     ids.append(next_id)
     print(next_id)
     # if next_id == tokenizer.token_to_id("[EOS]"):
