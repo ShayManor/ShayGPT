63b8bf1494edeaa4bfc453709b43ff7d7342af3c
shay <shay.manor@gmail.com>
2025-05-09 15:01:19 -0500

    increased batch size to 32

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ def get_args():
                    default=20)
     p.add_argument('--batch_size',
                    type=int,
-                   default=4)
+                   default=32)
     p.add_argument('--lr',
                    type=float,
                    default=8e-5)
