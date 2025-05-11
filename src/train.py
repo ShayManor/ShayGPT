d62848d9d4585403c5399415185491e8ebc89aca
shay <shay.manor@gmail.com>
2025-05-11 09:29:27 -0700

    params

--- src/train.py
+++ src/train.py
@@ -30,7 +30,7 @@ def get_args():
                    default=20)
     p.add_argument('--batch_size',
                    type=int,
-                   default=4)
+                   default=2)
     p.add_argument('--lr',
                    type=float,
                    default=1.5e-4)
