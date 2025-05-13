5d2de5fbe09f4dfba62136f34b37ace6e494d9b2
shay <shay.manor@gmail.com>
2025-05-12 20:50:09 -0700

    fix

--- src/train.py
+++ src/train.py
@@ -125,7 +125,7 @@ def train(resume: Optional[str],
     )
     wiki_ds = wiki_ds.rename_column("wikidata_id", "id")
     wiki_ds = wiki_ds.remove_columns(["version_id"])
-    wiki_ds = wiki_ds.cast(Features({"id": Value("string"), "text": Value("string")}))
+    wiki_ds = wiki_ds.cast(Features({"text": Value("string")}))
     owt_ds = load_dataset(
         "Skylion007/openwebtext",  # OpenWebText replication
         split="train",
